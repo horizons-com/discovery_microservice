0424e6e2234cf3b206b521783e6f2656016d7a34
Rachid Abadli <rabadli@localhost.localdomain>
2020-08-22 20:24:23 -0700

    Initial commit

---
+++ src/test/java/com/horizons/discovery_microservice/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horizons.discovery_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
